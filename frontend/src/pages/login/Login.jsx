5757295afea3b5e3689309c928c8d391853054eb
waynehuang-119 <waynehuang119@hotmail.com>
2024-06-17 21:29:28 -0700

    second part completed - ui design

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-35">
+        <h1 className="text-3xl font-semibold text-center text-gray-600">
+          Login
+          <span className="text-blue-500"> ChatApp</span>
+        </h1>
+
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter username"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+
+          <div>
+            <a
+              href="#"
+              className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+            >
+              {"Don't"} have an account?
+            </a>
+          </div>
+
+          <div>
+            <button className="btn btn-block btn-sm mt-2">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
